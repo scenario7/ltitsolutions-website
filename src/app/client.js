d48014d69993f377d3d2152cd67a4fa2862d8e58
kevinthomas <kevvin.thomas@gmail.com>
2024-07-03 00:40:30 +0530

    cache issue

--- src/app/client.js
+++ src/app/client.js
@@ -4,5 +4,5 @@ export default createClient({
   projectId: "98a8ncj5", // find this at manage.sanity.io or in your sanity.json
   dataset: "client-website-dataset", // this is from those question during 'sanity init'
   apiVersion: '2021-08-31',
-  useCdn: true
+  useCdn: false
 });
